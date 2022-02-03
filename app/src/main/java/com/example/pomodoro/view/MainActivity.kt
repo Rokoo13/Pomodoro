9c5097ee893b4db8b22b355b29364077abd8e1f0
Rokoo13 <gabrielsp1397@gmail.com>
2022-02-03 22:05:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pomodoro/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pomodoro.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pomodoro.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
