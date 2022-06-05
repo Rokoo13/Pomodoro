1bad5970b4610313e8bfa41d94e33fcdcdd7812b
gsp-dev-admin <gabrielsp1397@gmail.com>
2022-06-05 21:17:26 +0200

    POM-3 Creación de timer POM-4 Botón de inicio

--- app/src/main/java/com/example/pomodoro/view/MainActivity.kt
+++ app/src/main/java/com/example/pomodoro/view/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.pomodoro.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.example.pomodoro.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var contador : Unit
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    fun initView(){
+        start_counter.setOnClickListener {
+            contador = startCounter(1500000)
+        }
+    }
+    private fun startCounter(focusTime : Long){
+        object : CountDownTimer(focusTime,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                val segundosTot = (millisUntilFinished / 1000).toDouble()
+                var minSec = segundosTot / 60
+                val minutos =  (segundosTot / 60).toInt()
+                var segundos = (minSec - minutos) * 60
+                segundos = round(segundos)
+                timer.text = "$minutos : ${segundos.toInt()}"
+            }
+
+            override fun onFinish() {
+            }
+
+        }.start()
     }
 }
