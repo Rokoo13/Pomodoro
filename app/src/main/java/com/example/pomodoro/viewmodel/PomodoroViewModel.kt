5cd3a5dbad416a0a26e26c29d7317660caac8583
gsp-dev-admin <gabrielsp1397@gmail.com>
2022-06-23 20:40:02 +0200

    POM-13 Añadir alarma al terminar un contador

--- app/src/main/java/com/example/pomodoro/viewmodel/PomodoroViewModel.kt
+++ app/src/main/java/com/example/pomodoro/viewmodel/PomodoroViewModel.kt
@@ -1,13 +1,16 @@
 package com.example.pomodoro.viewmodel
 
+import android.media.MediaPlayer
 import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.pomodoro.PmdApplication
+import com.example.pomodoro.R
 import com.example.pomodoro.model.PomodoroModel
-import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.TimeUnit
-import kotlin.math.round
+
 
 class PomodoroViewModel : ViewModel() {
 
@@ -38,12 +41,15 @@ class PomodoroViewModel : ViewModel() {
                 val sec = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
                         TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))
                 setTimer(min.toInt(),sec.toInt(),pomodoroStart.restTimeMin, pomodoroStart.restTimeSec)
+                if(sec.toInt() == 0 && min.toInt() == 0){
+                    val mp: MediaPlayer = MediaPlayer.create(PmdApplication.getContext(), R.raw.rest_alarm)
+                    mp.start()
+                }
             }
 
             override fun onFinish() {
                 cancel()
-                startRestCounter()
-            }
+                Handler(Looper.getMainLooper()).postDelayed({ startRestCounter() }, 1000) }
 
         }.start()
     }
@@ -59,11 +65,15 @@ class PomodoroViewModel : ViewModel() {
                 val sec = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
                         TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))
                 setTimer(pomodoroStart.focusTimeMin, pomodoroStart.focusTimeSec,min.toInt(),sec.toInt())
+                if(sec.toInt() == 0 && min.toInt() == 0){
+                    val mp: MediaPlayer = MediaPlayer.create(PmdApplication.getContext(), R.raw.focus_alarm)
+                    mp.start()
+                }
             }
 
             override fun onFinish() {
                 cancel()
-                startCounter()
+                Handler(Looper.getMainLooper()).postDelayed({ startCounter() }, 1000)
             }
         }.start()
     }
